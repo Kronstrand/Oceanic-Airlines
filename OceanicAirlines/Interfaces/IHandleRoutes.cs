b4c4522a67e7a61964e62787d80082419a3a765c
Adrian_Stolarski <asto@netcompany.com>
2021-09-09 21:42:40 +0200

    Prepared basic search for route functionality

---
+++ OceanicAirlines/Interfaces/IHandleRoutes.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Oceanic_Airlines.Interfaces
+{
+    public interface IHandleRoutes
+    {
+        void PrepareDijkstra();
+        string PrepareKShortestPaths();
+    }
+
+}
